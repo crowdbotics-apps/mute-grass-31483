42336376a3954203a1bc5c32299d3e62e975bdf7
Crowdbotics <team@crowdbotics.com>
2021-10-16 17:45:15 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "mute_grass_31483",
+  url: "https://mute_grass_31483.botics.co",
+  api: "https://mute_grass_31483.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
